68fa6c8632ea4b27a5937859709883b785a0e87e
yeonseong shim <sim5522@naver.com>
2022-06-05 03:15:11 +0900

    Add Button, ImageList로 Start, Stop 토글 버튼 생성

--- Hello/Hello/Form1.Designer.cs
+++ Hello/Hello/Form1.Designer.cs
@@ -29,11 +29,49 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.startnstop = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.ImageIndex = 1;
+            this.button1.ImageList = this.startnstop;
+            this.button1.Location = new System.Drawing.Point(200, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 83);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "\r\nStart";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // startnstop
+            // 
+            this.startnstop.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.startnstop.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("startnstop.ImageStream")));
+            this.startnstop.TransparentColor = System.Drawing.Color.Transparent;
+            this.startnstop.Images.SetKeyName(0, "button01.jpg");
+            this.startnstop.Images.SetKeyName(1, "start.png");
+            this.startnstop.Images.SetKeyName(2, "stop.jpg");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(615, 367);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button button1;
+        private ImageList startnstop;
     }
 }
--- Hello/Hello/Form1.cs
+++ Hello/Hello/Form1.cs
@@ -1,3 +1,14 @@
+/*
+ * 1. Button 이벤트 등록
+ *  - ToolBox에서 Button을 드래그하여 폼에 끌어다 둔다.
+ * 
+ * 2. Button에 이미지 등록
+ *  - ToolBox에서 ImagList를 드래그하여 폼에 끌어다 둔다.
+ *  - ImageList를 클릭하여 속성창에서 Image에 이미지들을 넣는다.
+ *  - ImageList의 속성창에서 ImageSize를 변경하여 크기를 조정한다.
+ *  - Button 속성창에서 ImageList를 지정한다.
+ *  - ImageList 안에 포함된 이미지의 인덱스를 ImageIndex에 지정한다.
+ */
 namespace Hello
 {
     public partial class Form1 : Form
@@ -5,6 +16,25 @@ namespace Hello
         public Form1()
         {
             InitializeComponent();
+        }
+
+        int flag = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(flag == 0)
+            {
+                button1.ImageIndex = 2;
+                button1.Text = "Stop";
+                button1.BackColor = Color.Green;
+                flag = 1;
+            }
+            else
+            {
+                button1.ImageIndex = 1;
+                button1.Text = "Start";
+                button1.BackColor = Color.White;
+                flag = 0;
+            }
         }
     }
 }
