e39117069e76b209e6025fc755ae8e479a18a1cb
shimyseong@gmail.com <shimyseong@gmail.com>
2022-06-10 09:11:59 +0900

    Add TcpIp 통신 서버,클라이언트 프로그램

---
+++ Winform_Network/TcpIpCommunication/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace TcpIpCommunication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ClientIpTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ClientOpenBtn = new System.Windows.Forms.Button();
+            this.ClientSendBtn = new System.Windows.Forms.Button();
+            this.ClientMsgTB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ClientPortTB = new System.Windows.Forms.TextBox();
+            this.ClientCloseBtn = new System.Windows.Forms.Button();
+            this.ClientIpPortTB = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ClientDisplayTB = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ServerDisplayTB = new System.Windows.Forms.TextBox();
+            this.ServerIpPortTB = new System.Windows.Forms.TextBox();
+            this.ServerIpTB = new System.Windows.Forms.TextBox();
+            this.ServerCloseBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ServerOpenBtn = new System.Windows.Forms.Button();
+            this.ServerPortTB = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ClientIpTB
+            // 
+            this.ClientIpTB.Location = new System.Drawing.Point(74, 17);
+            this.ClientIpTB.Name = "ClientIpTB";
+            this.ClientIpTB.Size = new System.Drawing.Size(155, 21);
+            this.ClientIpTB.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server IP :";
+            // 
+            // ClientOpenBtn
+            // 
+            this.ClientOpenBtn.Location = new System.Drawing.Point(235, 44);
+            this.ClientOpenBtn.Name = "ClientOpenBtn";
+            this.ClientOpenBtn.Size = new System.Drawing.Size(45, 23);
+            this.ClientOpenBtn.TabIndex = 4;
+            this.ClientOpenBtn.Text = "Open";
+            this.ClientOpenBtn.UseVisualStyleBackColor = true;
+            this.ClientOpenBtn.Click += new System.EventHandler(this.ClientOpenBtn_Click);
+            // 
+            // ClientSendBtn
+            // 
+            this.ClientSendBtn.Location = new System.Drawing.Point(265, 73);
+            this.ClientSendBtn.Name = "ClientSendBtn";
+            this.ClientSendBtn.Size = new System.Drawing.Size(75, 21);
+            this.ClientSendBtn.TabIndex = 7;
+            this.ClientSendBtn.Text = "Send";
+            this.ClientSendBtn.UseVisualStyleBackColor = true;
+            this.ClientSendBtn.Click += new System.EventHandler(this.ClientSendBtn_Click);
+            // 
+            // ClientMsgTB
+            // 
+            this.ClientMsgTB.Location = new System.Drawing.Point(6, 73);
+            this.ClientMsgTB.Name = "ClientMsgTB";
+            this.ClientMsgTB.Size = new System.Drawing.Size(253, 21);
+            this.ClientMsgTB.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(235, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Port :";
+            // 
+            // ClientPortTB
+            // 
+            this.ClientPortTB.Location = new System.Drawing.Point(276, 17);
+            this.ClientPortTB.Name = "ClientPortTB";
+            this.ClientPortTB.Size = new System.Drawing.Size(64, 21);
+            this.ClientPortTB.TabIndex = 8;
+            // 
+            // ClientCloseBtn
+            // 
+            this.ClientCloseBtn.Location = new System.Drawing.Point(286, 44);
+            this.ClientCloseBtn.Name = "ClientCloseBtn";
+            this.ClientCloseBtn.Size = new System.Drawing.Size(54, 23);
+            this.ClientCloseBtn.TabIndex = 10;
+            this.ClientCloseBtn.Text = "Close";
+            this.ClientCloseBtn.UseVisualStyleBackColor = true;
+            this.ClientCloseBtn.Click += new System.EventHandler(this.ClientCloseBtn_Click);
+            // 
+            // ClientIpPortTB
+            // 
+            this.ClientIpPortTB.Enabled = false;
+            this.ClientIpPortTB.Location = new System.Drawing.Point(6, 46);
+            this.ClientIpPortTB.Name = "ClientIpPortTB";
+            this.ClientIpPortTB.Size = new System.Drawing.Size(223, 21);
+            this.ClientIpPortTB.TabIndex = 11;
+            this.ClientIpPortTB.Text = "IP와 Port를 설정하세요";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ClientDisplayTB);
+            this.groupBox1.Controls.Add(this.ClientMsgTB);
+            this.groupBox1.Controls.Add(this.ClientIpPortTB);
+            this.groupBox1.Controls.Add(this.ClientIpTB);
+            this.groupBox1.Controls.Add(this.ClientCloseBtn);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.ClientOpenBtn);
+            this.groupBox1.Controls.Add(this.ClientPortTB);
+            this.groupBox1.Controls.Add(this.ClientSendBtn);
+            this.groupBox1.Location = new System.Drawing.Point(27, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(347, 210);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Client";
+            // 
+            // ClientDisplayTB
+            // 
+            this.ClientDisplayTB.Location = new System.Drawing.Point(6, 100);
+            this.ClientDisplayTB.Multiline = true;
+            this.ClientDisplayTB.Name = "ClientDisplayTB";
+            this.ClientDisplayTB.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ClientDisplayTB.Size = new System.Drawing.Size(334, 104);
+            this.ClientDisplayTB.TabIndex = 12;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ServerDisplayTB);
+            this.groupBox2.Controls.Add(this.ServerIpPortTB);
+            this.groupBox2.Controls.Add(this.ServerIpTB);
+            this.groupBox2.Controls.Add(this.ServerCloseBtn);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.ServerOpenBtn);
+            this.groupBox2.Controls.Add(this.ServerPortTB);
+            this.groupBox2.Location = new System.Drawing.Point(27, 248);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(347, 199);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Server";
+            // 
+            // ServerDisplayTB
+            // 
+            this.ServerDisplayTB.Location = new System.Drawing.Point(6, 73);
+            this.ServerDisplayTB.Multiline = true;
+            this.ServerDisplayTB.Name = "ServerDisplayTB";
+            this.ServerDisplayTB.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ServerDisplayTB.Size = new System.Drawing.Size(334, 120);
+            this.ServerDisplayTB.TabIndex = 12;
+            // 
+            // ServerIpPortTB
+            // 
+            this.ServerIpPortTB.Enabled = false;
+            this.ServerIpPortTB.Location = new System.Drawing.Point(6, 46);
+            this.ServerIpPortTB.Name = "ServerIpPortTB";
+            this.ServerIpPortTB.Size = new System.Drawing.Size(223, 21);
+            this.ServerIpPortTB.TabIndex = 11;
+            this.ServerIpPortTB.Text = "IP와 Port를 설정하세요";
+            // 
+            // ServerIpTB
+            // 
+            this.ServerIpTB.Location = new System.Drawing.Point(74, 17);
+            this.ServerIpTB.Name = "ServerIpTB";
+            this.ServerIpTB.Size = new System.Drawing.Size(155, 21);
+            this.ServerIpTB.TabIndex = 0;
+            // 
+            // ServerCloseBtn
+            // 
+            this.ServerCloseBtn.Location = new System.Drawing.Point(286, 44);
+            this.ServerCloseBtn.Name = "ServerCloseBtn";
+            this.ServerCloseBtn.Size = new System.Drawing.Size(54, 23);
+            this.ServerCloseBtn.TabIndex = 10;
+            this.ServerCloseBtn.Text = "Close";
+            this.ServerCloseBtn.UseVisualStyleBackColor = true;
+            this.ServerCloseBtn.Click += new System.EventHandler(this.ServerCloseBtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Server IP :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(235, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Port :";
+            // 
+            // ServerOpenBtn
+            // 
+            this.ServerOpenBtn.Location = new System.Drawing.Point(235, 44);
+            this.ServerOpenBtn.Name = "ServerOpenBtn";
+            this.ServerOpenBtn.Size = new System.Drawing.Size(45, 23);
+            this.ServerOpenBtn.TabIndex = 4;
+            this.ServerOpenBtn.Text = "Open";
+            this.ServerOpenBtn.UseVisualStyleBackColor = true;
+            this.ServerOpenBtn.Click += new System.EventHandler(this.ServerOpenBtn_Click);
+            // 
+            // ServerPortTB
+            // 
+            this.ServerPortTB.Location = new System.Drawing.Point(276, 17);
+            this.ServerPortTB.Name = "ServerPortTB";
+            this.ServerPortTB.Size = new System.Drawing.Size(64, 21);
+            this.ServerPortTB.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 468);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ClientIpTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ClientOpenBtn;
+        private System.Windows.Forms.Button ClientSendBtn;
+        private System.Windows.Forms.TextBox ClientMsgTB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ClientPortTB;
+        private System.Windows.Forms.Button ClientCloseBtn;
+        private System.Windows.Forms.TextBox ClientIpPortTB;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox ClientDisplayTB;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox ServerDisplayTB;
+        private System.Windows.Forms.TextBox ServerIpPortTB;
+        private System.Windows.Forms.TextBox ServerIpTB;
+        private System.Windows.Forms.Button ServerCloseBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button ServerOpenBtn;
+        private System.Windows.Forms.TextBox ServerPortTB;
+    }
+}
+
---
+++ Winform_Network/TcpIpCommunication/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+
+namespace TcpIpCommunication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Form
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ServerIpTB.Text = GetLocalIP();
+            ServerPortTB.Text = Default_ServerPortNo.ToString();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region 전역변수
+        TcpListener listener;
+        string clientServerIp;
+        int clientServerPort;
+
+        const int Default_ServerPortNo = 7000;
+        const string Default_ServerIP = "127.0.0.1";
+        const int FirstServerConnect = 0;
+        const int ServerConnect = 1;
+        #endregion
+
+        #region 클라이언트
+        // Client ServerIP, Port 체크
+        private bool checkServerIPPORT()
+        {
+            if (clientServerPort != 0 && clientServerIp != "")
+                return true;
+            return false;
+        }
+
+        // Client Open btn이벤트
+        private void ClientOpenBtn_Click(object sender, EventArgs e)
+        {
+            clientServerIp = ClientIpTB.Text.Length == 0 ? "" : ClientIpTB.Text;
+            clientServerPort = ClientPortTB.Text.Length == 0 ? 0 : int.Parse(ClientPortTB.Text);
+
+            if (checkServerIPPORT())
+            {
+                // IP,Port 상태 업그레이드
+                ClientIpPortTB.Text = ClientIpTB.Text + "<" + int.Parse(ClientPortTB.Text) + ">";
+
+                try
+                {
+                    string msg = $"{GetLocalIP()}에서 서버에 접속하였습니다.";
+                    MsgProcess(clientServerIp, clientServerPort, msg, FirstServerConnect);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"서버 {clientServerIp}<{clientServerPort}>의 연결 상태를 확인하세요.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("ServerIp와 Port를 확인하세요.");
+            }
+
+
+        }
+
+        // Client Send btn이벤트
+        private void ClientSendBtn_Click(object sender, EventArgs e)
+        {
+            Client();
+        }
+
+        // Client와 Server 접속 체크
+        private void Client()
+        {
+            if (checkServerIPPORT())
+            {
+                try
+                {
+                    string msg = ClientMsgTB.Text;
+                    MsgProcess(clientServerIp, clientServerPort, msg, ServerConnect);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"서버 {clientServerIp}<{clientServerPort}>의 연결 상태를 확인하세요.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("ServerIp와 Port를 확인하세요.");
+            }
+        }
+
+        // Client에서 Server로 보내는 메세지
+        private void MsgProcess(string clientServerIp, int clientServerPort, string msg, int flag)
+        {
+            TcpClient client = new TcpClient(clientServerIp, clientServerPort);
+
+            byte[] buff = Encoding.UTF8.GetBytes(msg);
+
+            NetworkStream stream = client.GetStream();
+
+            // 스트림에 바이트 데이터 전송
+            stream.Write(buff, 0, buff.Length);
+
+            // 스트림으로부터 바이트 데이터 읽기
+            byte[] outbuf = new byte[1024];
+            int nbytes = stream.Read(outbuf, 0, outbuf.Length);
+            string output = Encoding.UTF8.GetString(outbuf);
+
+            if (flag == 0)
+            {
+                WriteClientDisplayTB($"{clientServerIp}<{clientServerPort}>에 접속하였습니다.");
+            }
+            else
+            {
+                WriteClientDisplayTB(msg);
+            }
+
+            stream.Close();
+            client.Close();
+
+
+            /*if (IsServerConnected(client))
+            {
+                
+            }
+            else
+            {
+                MessageBox.Show($"{clientServerIp}<{clientServerPort}>서버와 연결되어 있지 않습니다.");
+            }*/
+        }
+
+        // Client 출력창 메세지 표시
+        private void WriteClientDisplayTB(string msg)
+        {
+            ClientDisplayTB.Text += msg + "\r\n";
+        }
+
+        // Client Close btn 이벤트
+        private void ClientCloseBtn_Click(object sender, EventArgs e)
+        {
+            clientServerIp = "";
+            clientServerPort = 0;
+
+            // IP,Port 상태 업그레이드
+            ClientIpPortTB.Text = "";
+        }
+        #endregion
+
+        #region 서버
+        // Server Open btn 이벤트
+        private void ServerOpenBtn_Click(object sender, EventArgs e)
+        {
+            AsyncServer();
+        }
+
+        // Server 비동기 클라이언트 접속
+        private async Task AsyncServer()
+        {
+            // 서버 Default IP = 127.0.0.1, Default Port = 7000
+            IPAddress serverIp = ServerIpTB.Text.Length == 0 ? IPAddress.Parse(GetLocalIP()) : IPAddress.Parse(ServerIpTB.Text);
+            int serverPort = ServerPortTB.Text.Length == 0 ? Default_ServerPortNo : int.Parse(ServerPortTB.Text);
+
+            listener = new TcpListener(serverIp, serverPort);
+
+            // IP,Port 상태 업그레이드
+            ServerIpPortTB.Text = serverIp + "<" + serverPort + ">";
+
+            try
+            {
+                listener.Start();
+                MessageBox.Show("서버 시작됨");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            byte[] buff = new byte[1024];
+
+            while (true)
+            {
+                // Client의 요청을 받아들여 서버에서 새 TcpClient 객체를 생성하여 리턴
+                TcpClient client = await listener.AcceptTcpClientAsync().ConfigureAwait(false);
+
+                //ServerDisplayTB.Text = $"{client.Client.LocalEndPoint}, {client.Client.RemoteEndPoint}, {client.Client.LocalEndPoint.AddressFamily.ToString()}\r\n";
+
+                Task.Factory.StartNew(AsyncTcpProcess, client);
+            }
+        }
+
+        // Server 비동기 메세지 수신 및 송신
+        async void AsyncTcpProcess(object o)
+        {
+            TcpClient tc = (TcpClient)o;
+
+            int MAX_SIZE = 1024;
+            NetworkStream stream = tc.GetStream();
+            string msg = "";
+            var buff = new byte[MAX_SIZE];
+            var nbytes = await stream.ReadAsync(buff, 0, buff.Length).ConfigureAwait(false);
+            if (nbytes > 0)
+            {
+                msg = Encoding.UTF8.GetString(buff, 0, nbytes);
+
+                await stream.WriteAsync(buff, 0, nbytes).ConfigureAwait(false);
+            }
+
+            WriteServerDisplayTB(msg);
+
+            stream.Close();
+        }
+
+        // Server 출력창 메세지 표시
+        private void WriteServerDisplayTB(string msg)
+        {
+            ServerDisplayTB.Invoke((MethodInvoker)delegate
+            {
+                ServerDisplayTB.Text += msg + "\r\n";
+            });
+        }
+
+
+
+        // Server Cloese btn 이벤트
+        private void ServerCloseBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listener.Stop();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            // IP,Port 상태 업그레이드
+            ServerIpPortTB.Text = "";
+        }
+        #endregion
+
+        #region 유틸
+        // 로컬 IP 가져오는 메소드
+        private string GetLocalIP()
+        {
+            string localIP = string.Empty;
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
+            }
+            return localIP;
+        }
+
+        // 현재 연결중인 TCP의 연결 상태를 가졍는 메소드
+        private bool IsServerConnected(TcpClient client)
+        {
+            // 로컬 PC에 연결된 모든 TCP 커넥션 정보를 가져온다.
+            IPGlobalProperties ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+
+            TcpConnectionInformation[] tcpConnections;
+
+            try
+            {
+                // 특정 IP와의 통신 상태 여부를 위해 받아온 client의 연결정보를 가져온다.
+                tcpConnections = ipProperties.GetActiveTcpConnections()
+                    .Where(x => x.LocalEndPoint.Equals(client.Client.LocalEndPoint) && x.RemoteEndPoint.Equals(client.Client.RemoteEndPoint))
+                    .ToArray();
+            }
+            catch
+            {
+                // 서버와 연결 안됨
+                return false;
+            }
+
+            if (tcpConnections != null && tcpConnections.Length > 0)
+            {
+                // TCP 상태 가져오기
+                TcpState stateOfConnection = tcpConnections.First().State;
+                if (stateOfConnection == TcpState.Established)
+                    return true;
+                else
+                    return false;
+            }
+
+            // 서버와 연결 안됨
+            return false;
+        }
+        #endregion
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
