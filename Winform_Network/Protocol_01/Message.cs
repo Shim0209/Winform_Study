f266db0327c4f148d9105edb40e8451df22043e6
yeonseong shim <shimyseong@gmail.com>
2022-06-12 02:26:04 +0900

    Add Protocol_01 프로토콜 설계 및 Message 구조 구현.

---
+++ Winform_Network/Protocol_01/Message.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protocol_01
+{
+    public class CONSTANTS
+    {
+        public const string SUCCESS = "ACK";
+        public const string FAIL = "NCK";
+
+        enum PICKER_ITEM
+        {
+            P0, P1, P2, P3, P4, P5
+        }
+
+        enum ROTATION_ITEM
+        {
+            ROTATION1, ROTATION2, ROTATION3
+        }
+    }
+
+    public interface ISerializable 
+    {
+        byte[] GetBytes();
+        int GetSize();
+    }
+
+    public class Message : ISerializable
+    {
+        public string STX { get; set; }
+        public string ETX { get; set; }
+        public Base Base { get; set; }
+        public Data? Data { get; set; }
+        public Error? Error { get; set; }
+        public Value? Value { get; set; }
+
+        public Message()
+        {
+        }
+
+        public Message(Base @base)
+        {
+            Base = @base;
+        }
+
+        public Message(Base @base, Data? data) : this(@base)
+        {
+            Data = data;
+        }
+
+        public Message(Base @base, Data? data, Error? error) : this(@base, data)
+        {
+            Error = error;
+        }
+
+        public Message(Base @base, Data? data, Value? value) : this(@base, data)
+        {
+            Value = value;
+        }
+
+        public byte[] GetBytes()
+        {
+            byte[] bytes = new byte[GetSize()];
+
+            if (Value != null)
+            {
+                Encoding.UTF8.GetBytes(STX).CopyTo(bytes, 0);
+                Base.GetBytes().CopyTo(bytes, STX.Length);
+                Data.GetBytes().CopyTo(bytes, STX.Length + Base.GetSize());
+                Value.GetBytes().CopyTo(bytes, STX.Length + Base.GetSize() + Data.GetSize());
+                Encoding.UTF8.GetBytes(ETX).CopyTo(bytes, STX.Length + Base.GetSize() + Data.GetSize() + Value.GetSize());
+                return bytes;
+            }
+            else if (Error != null)
+            {
+                Encoding.UTF8.GetBytes(STX).CopyTo(bytes, 0);
+                Base.GetBytes().CopyTo(bytes, STX.Length);
+                Data.GetBytes().CopyTo(bytes, STX.Length + Base.GetSize());
+                Error.GetBytes().CopyTo(bytes, STX.Length + Base.GetSize() + Data.GetSize());
+                Encoding.UTF8.GetBytes(ETX).CopyTo(bytes, STX.Length + Base.GetSize() + Data.GetSize() + Error.GetSize());
+                return bytes;
+            }
+            else if (Data != null)
+            {
+                Encoding.UTF8.GetBytes(STX).CopyTo(bytes, 0);
+                Base.GetBytes().CopyTo(bytes, STX.Length);
+                Data.GetBytes().CopyTo(bytes, STX.Length + Base.GetSize());
+                Encoding.UTF8.GetBytes(ETX).CopyTo(bytes, STX.Length + Base.GetSize() + Data.GetSize());
+                return bytes;
+            }
+            else
+            {
+                Encoding.UTF8.GetBytes(STX).CopyTo(bytes, 0);
+                Base.GetBytes().CopyTo(bytes, STX.Length);
+                Encoding.UTF8.GetBytes(ETX).CopyTo(bytes, STX.Length + Base.GetSize());
+                return bytes;
+            }
+        }
+
+        public int GetSize()
+        {
+            if(Value != null)
+            {
+                return Base.GetSize() + Data.GetSize() + Value.GetSize();
+            }
+            else if(Error != null)
+            {
+                return Base.GetSize() + Data.GetSize() + Error.GetSize();
+            }
+            else if(Data != null)
+            {
+                return Base.GetSize() + Data.GetSize();
+            }
+            else
+            {
+                return Base.GetSize();
+            }
+        }
+    }
+
+    public class Base : ISerializable
+    {
+        // P0                   : 인터페이스
+        // P1, P2, P3, P4, P5   : P 번호
+        public string PICKER { get; set; }
+        public string VISION { get; set; }
+        public string COMMAND { get; set; }
+        public byte[] GetBytes()
+        {
+            string baseStr = PICKER + VISION + COMMAND;
+            return Encoding.UTF8.GetBytes(baseStr);
+        }
+
+        public int GetSize()
+        {
+            return PICKER.Length + VISION.Length + COMMAND.Length;
+        }
+    }
+
+    public class Data : ISerializable
+    {
+        public string innerData { get; set; }
+        public byte[] GetBytes()
+        {
+            return Encoding.UTF8.GetBytes(innerData);
+        }
+
+        public int GetSize()
+        {
+            return innerData.Length;
+        }
+    }
+
+    public class Error : ISerializable
+    {
+        public string innerData { get; set; }
+        public byte[] GetBytes()
+        {
+            return Encoding.UTF8.GetBytes(innerData);
+        }
+
+        public int GetSize()
+        {
+            return innerData.Length;
+        }
+    }
+
+    public class Value : ISerializable
+    {
+        string X { get; set; }
+        string Y { get; set; }
+        string Z { get; set; }
+        public byte[] GetBytes()
+        {
+            string valueStr = X + Y + Z;
+            return Encoding.UTF8.GetBytes(valueStr);
+        }
+
+        public int GetSize()
+        {
+            return X.Length + Y.Length + Z.Length;
+        }
+    }
+}
---
+++ Winform_Network/Protocol_01/MessageUtil.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protocol_01
+{
+    class MessageUtil
+    {
+        public static void Send(Stream stream, Message message)
+        {
+            stream.Write(message.GetBytes());
+        }
+
+        // 수신 -> COMMAND분석 -> COM에 따른 처리 메소드호출 -> 응답할 메세지 반환
+        async public static Task<Message> Receive(Stream stream)
+        {
+            int MAX_SIZE = 1024;
+            string msg = "";
+            Task<Message> respMsg = null;
+            var buff = new byte[MAX_SIZE];  
+            var nbytes = await stream.ReadAsync(buff, 0, buff.Length).ConfigureAwait(false);
+            if(nbytes > 0)
+            {
+                msg = Encoding.UTF8.GetString(buff, 0, nbytes).Substring(1, msg.Length);
+
+                // 받은메세지 로그에 출력
+                writeLog(msg, "rec");
+
+                msg.Substring(1, msg.Length); // STX, ETX 제거
+                string[] splitMsg = msg.Split(","); // ,로 프로토콜 요소 분리
+
+
+                // COMMAND 별로 각 메소드 호출해서 응답할 값을 받아온다.
+                // respMsg = StartResp();
+                switch (splitMsg[2].Substring(0,3))
+                {
+                    case "STA":
+                        respMsg = GetStartResp(splitMsg);
+                        break;
+                    case "END":
+                        respMsg = GetEndResp(splitMsg);
+                        break;
+                    case "REQ":
+                        respMsg = GetRequestResp(splitMsg);
+                        break;
+                    case "ROT":
+                        respMsg = GetRotationResp(splitMsg);
+                        break;
+                }
+            }
+
+            // 보낼메세지 로그에 출력
+            writeLog(respMsg.ToString(), "resp");
+
+            // 송신자에게 결과 메세지 응답
+            Send(stream, respMsg.Result);
+
+            stream.Close();
+
+            return respMsg.Result;
+        }
+
+        async public static Task<Message> GetStartResp(string[] message)
+        {
+            // @@@구현해야함
+            return new Message();
+        }
+
+        async public static Task<Message> GetEndResp(string[] message)
+        {
+            // @@@구현해야함
+            return new Message();
+        }
+
+        async public static Task<Message> GetRequestResp(string[] message)
+        {
+            // @@@구현해야함
+            return new Message();
+        }
+
+        async public static Task<Message> GetRotationResp(string[] message)
+        {
+            // @@@구현해야함
+            return new Message();
+        }
+
+        // 받은메세지 rev, 보낼메세지 resp
+        public static void writeLog(string message, string flag)
+        {
+            // 받은 메시지, 보낼 메세지 구별해서 로그에 출력
+            if(flag == "rev")
+            {
+                // @@@구현해야함
+            }
+            else if(flag == "resp")
+            {
+                // @@@구현해야함
+            }
+        }
+    }
+}
