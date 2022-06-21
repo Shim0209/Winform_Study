a6023acbaaadca0128693e96cb239cd0c5324260
shimyseong@gmail.com <shimyseong@gmail.com>
2022-06-21 14:36:08 +0900

    Add MetroUI 프로젝트

---
+++ Winform_MetroUI/HelloMetroUI/HelloMetroUI/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloMetroUI
+{
+    // MetroUI 설치
+    // 1. 
+    public partial class Form1 : MetroFramework.Forms.MetroForm // 상속 클래스 변경
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
