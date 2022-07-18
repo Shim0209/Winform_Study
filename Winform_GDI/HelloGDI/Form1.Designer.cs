6445309debb60fb95dfa449962d7506db432b592
shimyseong@gmail.com <shimyseong@gmail.com>
2022-07-18 15:25:23 +0900

    Update GDI 기본 기능

--- Winform_GDI/HelloGDI/Form1.Designer.cs
+++ Winform_GDI/HelloGDI/Form1.Designer.cs
@@ -28,13 +28,173 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Btn_DrawLine = new System.Windows.Forms.Button();
+            this.Btn_FormClear = new System.Windows.Forms.Button();
+            this.Btn_DrawEllipse = new System.Windows.Forms.Button();
+            this.Btn_DrawRectangle = new System.Windows.Forms.Button();
+            this.Btn_DrawString = new System.Windows.Forms.Button();
+            this.Btn_DrawImage = new System.Windows.Forms.Button();
+            this.Pn_DrawImage = new System.Windows.Forms.Panel();
+            this.Btn_DrawFillEllipse = new System.Windows.Forms.Button();
+            this.Btn_DrawFillRectangle = new System.Windows.Forms.Button();
+            this.Btn_CreateNewRegion = new System.Windows.Forms.Button();
+            this.Btn_Test = new System.Windows.Forms.Button();
+            this.Btn_CreateNewRectangle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn_DrawLine
+            // 
+            this.Btn_DrawLine.Location = new System.Drawing.Point(563, 12);
+            this.Btn_DrawLine.Name = "Btn_DrawLine";
+            this.Btn_DrawLine.Size = new System.Drawing.Size(161, 23);
+            this.Btn_DrawLine.TabIndex = 0;
+            this.Btn_DrawLine.Text = "DrawLine";
+            this.Btn_DrawLine.UseVisualStyleBackColor = true;
+            this.Btn_DrawLine.Click += new System.EventHandler(this.Btn_DrawLine_Click);
+            // 
+            // Btn_FormClear
+            // 
+            this.Btn_FormClear.Location = new System.Drawing.Point(563, 315);
+            this.Btn_FormClear.Name = "Btn_FormClear";
+            this.Btn_FormClear.Size = new System.Drawing.Size(161, 23);
+            this.Btn_FormClear.TabIndex = 1;
+            this.Btn_FormClear.Text = "Clear";
+            this.Btn_FormClear.UseVisualStyleBackColor = true;
+            this.Btn_FormClear.Click += new System.EventHandler(this.Btn_FormClear_Click);
+            // 
+            // Btn_DrawEllipse
+            // 
+            this.Btn_DrawEllipse.Location = new System.Drawing.Point(563, 41);
+            this.Btn_DrawEllipse.Name = "Btn_DrawEllipse";
+            this.Btn_DrawEllipse.Size = new System.Drawing.Size(161, 23);
+            this.Btn_DrawEllipse.TabIndex = 2;
+            this.Btn_DrawEllipse.Text = "DrawEllipse";
+            this.Btn_DrawEllipse.UseVisualStyleBackColor = true;
+            this.Btn_DrawEllipse.Click += new System.EventHandler(this.Btn_DrawEllipse_Click);
+            // 
+            // Btn_DrawRectangle
+            // 
+            this.Btn_DrawRectangle.Location = new System.Drawing.Point(563, 99);
+            this.Btn_DrawRectangle.Name = "Btn_DrawRectangle";
+            this.Btn_DrawRectangle.Size = new System.Drawing.Size(161, 23);
+            this.Btn_DrawRectangle.TabIndex = 3;
+            this.Btn_DrawRectangle.Text = "DrawRectangle";
+            this.Btn_DrawRectangle.UseVisualStyleBackColor = true;
+            this.Btn_DrawRectangle.Click += new System.EventHandler(this.Btn_DrawRectangle_Click);
+            // 
+            // Btn_DrawString
+            // 
+            this.Btn_DrawString.Location = new System.Drawing.Point(563, 157);
+            this.Btn_DrawString.Name = "Btn_DrawString";
+            this.Btn_DrawString.Size = new System.Drawing.Size(161, 23);
+            this.Btn_DrawString.TabIndex = 4;
+            this.Btn_DrawString.Text = "DrawString";
+            this.Btn_DrawString.UseVisualStyleBackColor = true;
+            this.Btn_DrawString.Click += new System.EventHandler(this.Btn_DrawString_Click);
+            // 
+            // Btn_DrawImage
+            // 
+            this.Btn_DrawImage.Location = new System.Drawing.Point(563, 186);
+            this.Btn_DrawImage.Name = "Btn_DrawImage";
+            this.Btn_DrawImage.Size = new System.Drawing.Size(161, 23);
+            this.Btn_DrawImage.TabIndex = 5;
+            this.Btn_DrawImage.Text = "DrawImage";
+            this.Btn_DrawImage.UseVisualStyleBackColor = true;
+            this.Btn_DrawImage.Click += new System.EventHandler(this.Btn_DrawImage_Click);
+            // 
+            // Pn_DrawImage
+            // 
+            this.Pn_DrawImage.Location = new System.Drawing.Point(315, 12);
+            this.Pn_DrawImage.Name = "Pn_DrawImage";
+            this.Pn_DrawImage.Size = new System.Drawing.Size(242, 326);
+            this.Pn_DrawImage.TabIndex = 6;
+            // 
+            // Btn_DrawFillEllipse
+            // 
+            this.Btn_DrawFillEllipse.Location = new System.Drawing.Point(563, 70);
+            this.Btn_DrawFillEllipse.Name = "Btn_DrawFillEllipse";
+            this.Btn_DrawFillEllipse.Size = new System.Drawing.Size(161, 23);
+            this.Btn_DrawFillEllipse.TabIndex = 7;
+            this.Btn_DrawFillEllipse.Text = "DrawFillEllipse";
+            this.Btn_DrawFillEllipse.UseVisualStyleBackColor = true;
+            this.Btn_DrawFillEllipse.Click += new System.EventHandler(this.Btn_DrawFillEllipse_Click);
+            // 
+            // Btn_DrawFillRectangle
+            // 
+            this.Btn_DrawFillRectangle.Location = new System.Drawing.Point(563, 128);
+            this.Btn_DrawFillRectangle.Name = "Btn_DrawFillRectangle";
+            this.Btn_DrawFillRectangle.Size = new System.Drawing.Size(161, 23);
+            this.Btn_DrawFillRectangle.TabIndex = 8;
+            this.Btn_DrawFillRectangle.Text = "DrawFillRectangle";
+            this.Btn_DrawFillRectangle.UseVisualStyleBackColor = true;
+            this.Btn_DrawFillRectangle.Click += new System.EventHandler(this.Btn_DrawFillRectangle_Click);
+            // 
+            // Btn_CreateNewRegion
+            // 
+            this.Btn_CreateNewRegion.Location = new System.Drawing.Point(563, 215);
+            this.Btn_CreateNewRegion.Name = "Btn_CreateNewRegion";
+            this.Btn_CreateNewRegion.Size = new System.Drawing.Size(161, 23);
+            this.Btn_CreateNewRegion.TabIndex = 9;
+            this.Btn_CreateNewRegion.Text = "CreateNewRegion";
+            this.Btn_CreateNewRegion.UseVisualStyleBackColor = true;
+            this.Btn_CreateNewRegion.Click += new System.EventHandler(this.Btn_CreateNewRegion_Click);
+            // 
+            // Btn_Test
+            // 
+            this.Btn_Test.Location = new System.Drawing.Point(211, 315);
+            this.Btn_Test.Name = "Btn_Test";
+            this.Btn_Test.Size = new System.Drawing.Size(98, 23);
+            this.Btn_Test.TabIndex = 10;
+            this.Btn_Test.Text = "Test Button";
+            this.Btn_Test.UseVisualStyleBackColor = true;
+            // 
+            // Btn_CreateNewRectangle
+            // 
+            this.Btn_CreateNewRectangle.Location = new System.Drawing.Point(563, 244);
+            this.Btn_CreateNewRectangle.Name = "Btn_CreateNewRectangle";
+            this.Btn_CreateNewRectangle.Size = new System.Drawing.Size(161, 23);
+            this.Btn_CreateNewRectangle.TabIndex = 11;
+            this.Btn_CreateNewRectangle.Text = "CreateNewRectangle";
+            this.Btn_CreateNewRectangle.UseVisualStyleBackColor = true;
+            this.Btn_CreateNewRectangle.Click += new System.EventHandler(this.Btn_CreateNewRectangle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(729, 344);
+            this.Controls.Add(this.Btn_CreateNewRectangle);
+            this.Controls.Add(this.Btn_Test);
+            this.Controls.Add(this.Btn_CreateNewRegion);
+            this.Controls.Add(this.Btn_DrawFillRectangle);
+            this.Controls.Add(this.Btn_DrawFillEllipse);
+            this.Controls.Add(this.Pn_DrawImage);
+            this.Controls.Add(this.Btn_DrawImage);
+            this.Controls.Add(this.Btn_DrawString);
+            this.Controls.Add(this.Btn_DrawRectangle);
+            this.Controls.Add(this.Btn_DrawEllipse);
+            this.Controls.Add(this.Btn_FormClear);
+            this.Controls.Add(this.Btn_DrawLine);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Btn_DrawLine;
+        private System.Windows.Forms.Button Btn_FormClear;
+        private System.Windows.Forms.Button Btn_DrawEllipse;
+        private System.Windows.Forms.Button Btn_DrawRectangle;
+        private System.Windows.Forms.Button Btn_DrawString;
+        private System.Windows.Forms.Button Btn_DrawImage;
+        private System.Windows.Forms.Panel Pn_DrawImage;
+        private System.Windows.Forms.Button Btn_DrawFillEllipse;
+        private System.Windows.Forms.Button Btn_DrawFillRectangle;
+        private System.Windows.Forms.Button Btn_CreateNewRegion;
+        private System.Windows.Forms.Button Btn_Test;
+        private System.Windows.Forms.Button Btn_CreateNewRectangle;
     }
 }
 
--- Winform_GDI/HelloGDI/Form1.cs
+++ Winform_GDI/HelloGDI/Form1.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// [[ Graphics객체를 만드는 방법 ]]
+//      1. Form의 Paint 이벤트
+//          Graphics객체를 만들어 PaintEventArgs의 Graphics를 참조
+//          graphics = e.Graphics;
+//      2. CreateGraphics 메서드
+//          g = this.CreateGraphics();
+//      3. 이미지 개체에서 만들기
+//          Bitmap myBitmap = new Bitmap(@"C:\Documents and Settings\Joe\Pics\myPic.bmp");
+//          Graphics g = Graphics.FromImage(myBitmap);
+//
+// [[ Graphics객체로 도형, 이미지 그리기 및 조작 ]]
+//      pen 클래스 - 선을 그리거나, 도형을 간략하게 표시하거나, 다른 기하학적 표현을 렌더링하는데 사용됨.
+//      Brush 클래스 - 채워진 도형, 이미지 또는 텍스트와 같은 그래픽 영역을 채우는데 사용됨.
+//      Font 클래스 - 텍스트를 렌더링할 때 사용할 도형에 대한 설명을 제공함.
+//      Color 클래스 - 표시할 다른 색을 나타냄.
+//
+// [[ 리소스 해제 ]]
+//      시스템 리소스를 사용하는 모든 객체에서 항상 Dispose를 출력해야 한다.
 namespace HelloGDI
 {
     public partial class Form1 : Form
@@ -16,5 +35,209 @@ namespace HelloGDI
         {
             InitializeComponent();
         }
+
+        #region FormClear
+        private void Btn_FormClear_Click(object sender, EventArgs e)
+        {
+            Graphics FormGraphics = this.CreateGraphics();
+            Graphics PanelGraphics = Pn_DrawImage.CreateGraphics();
+
+            FormGraphics.Clear(this.BackColor);
+            PanelGraphics.Clear(this.BackColor);
+
+            FormGraphics.Dispose();
+            PanelGraphics.Dispose();
+        }
+        #endregion
+
+        #region DrawLine
+        private void Btn_DrawLine_Click(object sender, EventArgs e)
+        {
+            DrawLine();
+        }
+        private void DrawLine()
+        {
+            // 생성
+            Graphics graphics = this.CreateGraphics();
+            Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0));
+            
+            // 그리기
+            graphics.DrawLine(pen, 20, 10, 300, 100);
+
+            // 해제
+            pen.Dispose();
+            graphics.Dispose();
+        }
+        #endregion
+
+        #region DrawEllipse
+        private void Btn_DrawEllipse_Click(object sender, EventArgs e)
+        {
+            DrawEllipse("Default");
+        }
+        private void Btn_DrawFillEllipse_Click(object sender, EventArgs e)
+        {
+            DrawEllipse("Fill");
+        }
+        private void DrawEllipse(string flag)
+        {
+            Graphics graphics = this.CreateGraphics();
+            Pen pen;
+            Brush brush;
+
+            if(flag == "Default")
+            {
+                pen = new Pen(Color.DarkBlue);
+                graphics.DrawEllipse(pen, new Rectangle(0, 0, 200, 300));
+                pen.Dispose();
+            }
+            else if(flag == "Fill")
+            {
+                brush = new SolidBrush(Color.DarkBlue);
+                graphics.FillEllipse(brush, new Rectangle(0, 0, 200, 300));
+                brush.Dispose();
+            }
+
+            graphics.Dispose();
+        }
+        #endregion
+
+        #region DrawRectangle
+        private void Btn_DrawRectangle_Click(object sender, EventArgs e)
+        {
+            DrawRectangle("Default");
+        }
+        private void Btn_DrawFillRectangle_Click(object sender, EventArgs e)
+        {
+            DrawRectangle("Fill");
+        }
+        private void DrawRectangle(string flag)
+        {
+            Graphics graphics = this.CreateGraphics();
+            Pen pen;
+            Brush brush;
+
+            // 그리기
+            if(flag == "Default")
+            {
+                pen = new Pen(Color.Red);
+                graphics.DrawRectangle(pen, new Rectangle(0, 0, 200, 300));
+                pen.Dispose();
+            }
+            else if(flag == "Fill")
+            {
+                brush = new SolidBrush(Color.Red);
+                graphics.FillRectangle(brush, new Rectangle(0, 0, 200, 300));
+                brush.Dispose();
+            }
+
+            graphics.Dispose();
+        }
+        
+        #endregion
+
+        #region DrawString
+        private void Btn_DrawString_Click(object sender, EventArgs e)
+        {
+            DrawString();
+        }
+        private void DrawString()
+        {
+            // 생성
+            Graphics graphics = this.CreateGraphics();
+            Font font = new Font("Arial", 16);
+            Brush brush = new SolidBrush(Color.Black);
+            StringFormat stringFormat = new StringFormat();
+
+            // 준비
+            string text = "Sample Text";
+            float x = 150.0F;
+            float y = 50.0F;
+
+            // 그리기
+            graphics.DrawString(text, font, brush, x, y, stringFormat);
+
+            // 해제
+            stringFormat.Dispose();
+            brush.Dispose();
+            font.Dispose();
+            graphics.Dispose();
+        }
+        #endregion
+
+        #region DrawImage
+        private void Btn_DrawImage_Click(object sender, EventArgs e)
+        {
+            DrawImage();
+        }
+
+        private void DrawImage()
+        {
+            // 생성
+            Graphics graphics = Pn_DrawImage.CreateGraphics(); // Panel의 Graphics로 생성
+            Bitmap bitmap = new Bitmap($"C:\\Users\\DPS7\\Desktop\\dd2.png");
+
+            // 그리기
+            graphics.DrawImage(bitmap, 1, 1);
+
+            // 해제
+            bitmap.Dispose();
+            graphics.Dispose();
+        }
+
+
+        #endregion
+
+        #region CreateNewRegion
+        private void Btn_CreateNewRegion_Click(object sender, EventArgs e)
+        {
+            CreateNewRegion();
+        }
+        private void CreateNewRegion()
+        {
+            GraphicsPath shape = new GraphicsPath();
+            shape.AddEllipse(0, 0, this.Width, this.Height);
+            this.Region = new Region(shape);
+
+            shape.Dispose();
+        }
+        #endregion
+
+        #region CreateNewRectangle
+        private void Btn_CreateNewRectangle_Click(object sender, EventArgs e)
+        {
+            CreateNewRectangle();
+        }
+
+        private void CreateNewRectangle()
+        {
+            Size Sqare = new Size(6, 6);
+
+            /*int X = Btn_Test.Bounds.X - Sqare.Width;
+            int Y = Btn_Test.Bounds.Y - Sqare.Height;
+            int Height = Btn_Test.Bounds.Height + (Sqare.Height * 2);
+            int Width = Btn_Test.Bounds.Width + (Sqare.Width * 2);
+
+            this.Bounds = new Rectangle(X, Y, Width + 1, Height + 1);
+
+            this.BringToFront();*/
+
+            GraphicsPath shape = new GraphicsPath();
+
+            // 두께가 있는 작은 사각형을 좌우상하로 만들어 배치 (선의 두께를 위해서?)
+            // Top
+            shape.AddRectangle(new Rectangle(0,0, Btn_Test.Width + (Sqare.Width*2)+1, Sqare.Height+1));
+            // Left
+            shape.AddRectangle(new Rectangle(0, Sqare.Height+1, Sqare.Width + 1, Btn_Test.Bounds.Height + Sqare.Height+1));
+            // Bottom
+            shape.AddRectangle(new Rectangle(Sqare.Width + 1, Btn_Test.Bounds.Height + Sqare.Height + 1, Btn_Test.Width, Sqare.Height + 1));
+            // Right
+            shape.AddRectangle(new Rectangle(Btn_Test.Width + Sqare.Width, Sqare.Height+1, Sqare.Width+1, Btn_Test.Bounds.Height+ Sqare.Height+1));
+
+            this.Region = new Region(shape);
+
+            shape.Dispose();
+        }
+        #endregion
     }
 }
