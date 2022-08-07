6e33022326cac0cf91225c74ae9c376c236faf4f
yeonseong shim <shimyseong@gmail.com>
2022-08-07 23:10:06 +0900

    Update 동적레이아웃

---
+++ Winform_Basic/DynamicLayout/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace DynamicLayout
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.ucPanel1 = new DynamicLayout.ucPanel();
+            this.ucPanel2 = new DynamicLayout.ucPanel();
+            this.ucPanel3 = new DynamicLayout.ucPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.ucPanel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ucPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ucPanel3, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // ucPanel1
+            // 
+            this.ucPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ucPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucPanel1.Location = new System.Drawing.Point(0, 0);
+            this.ucPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.ucPanel1.Name = "ucPanel1";
+            this.tableLayoutPanel1.SetRowSpan(this.ucPanel1, 2);
+            this.ucPanel1.Size = new System.Drawing.Size(100, 450);
+            this.ucPanel1.TabIndex = 0;
+            // 
+            // ucPanel2
+            // 
+            this.ucPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ucPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucPanel2.Location = new System.Drawing.Point(100, 0);
+            this.ucPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.ucPanel2.Name = "ucPanel2";
+            this.ucPanel2.Size = new System.Drawing.Size(700, 70);
+            this.ucPanel2.TabIndex = 1;
+            // 
+            // ucPanel3
+            // 
+            this.ucPanel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ucPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucPanel3.Location = new System.Drawing.Point(103, 73);
+            this.ucPanel3.Name = "ucPanel3";
+            this.ucPanel3.Size = new System.Drawing.Size(694, 374);
+            this.ucPanel3.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private ucPanel ucPanel1;
+        private ucPanel ucPanel2;
+        private ucPanel ucPanel3;
+    }
+}
+
---
+++ Winform_Basic/DynamicLayout/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Forms;
+
+// 동적 Layout
+// - Monitor 및 Window Tab 등 Display Device의 종류가 다양해 짐에 따라 화면 비율을 동적으로 변환 시킬 필요가 생김
+// - TableLayoutPanel
+//      Talbe 형태의 Layout을 지정
+// - SplitContainer   
+//      이동이 가능 한 Split Panel을 지정
+namespace DynamicLayout
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
+    }
+}
