e2a9ba09697bf2fda82aac89ffb1afa3f69c98f8
yeonseong shim <shimyseong@gmail.com>
2022-09-15 02:39:04 +0900

    Update MetroUI 연습 및 SplashScreen 학습

---
+++ Winform_MetroUI/HelloMetroUI/HelloMetroUI/frmMain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloMetroUI
+{
+    public partial class frmMain : MetroFramework.Forms.MetroForm
+    {
+        Thread t;
+        public frmMain()
+        {
+            t = new Thread(new ThreadStart(Loading));
+            t.Start();
+            InitializeComponent();
+        }
+
+        void Loading()
+        {
+            frmSplashScreen frm = new frmSplashScreen();
+            Application.Run(frm);
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // 임시 조건
+            for (int i = 0; i <= 1000; i++)
+                Thread.Sleep(10);
+
+            // Complete
+            // 프로그램 시작에 필요한 모든게 로딩 완료되면
+            // 아래 명령어 실행하여 SplashScreen을 종료하고 Main을 업시킨다.
+            t.Abort(); 
+        }
+    }
+}
--- Winform_MetroUI/HelloMetroUI/HelloMetroUI/frmSplashScreen.cs
+++ Winform_MetroUI/HelloMetroUI/HelloMetroUI/frmSplashScreen.cs
@@ -11,10 +11,9 @@ using System.Windows.Forms;
 namespace HelloMetroUI
 {
     // MetroUI 설치
-    // 1. 
-    public partial class Form1 : MetroFramework.Forms.MetroForm // 상속 클래스 변경
+    public partial class frmSplashScreen : MetroFramework.Forms.MetroForm // 상속 클래스 변경
     {
-        public Form1()
+        public frmSplashScreen()
         {
             InitializeComponent();
         }
